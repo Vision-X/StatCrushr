2e520f253c6c686168b13d84c80596b7cba2a320
Vision-X <michael.s.marlow@gmail.com>
2018-06-28 13:08:00 -0600

    just getting basic piece deployed for server side struggles with oauth2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// const ClientOAuth2 = require("client-oauth2");
+// const JUNK = require('./.env');
+//
+// const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+// const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+
+// $url = 'https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nfl/teams';
+// $scope = 'test';
 
 class App extends Component {
+  constructor() {
+  super();
+  this.state = {};
+  }
+
+  yahooAuth = () => {
+    // console.log(CLIENT_ID);
+    var yAuth = new ClientOAuth2({
+      clientId: `${CLIENT_ID}`
+    });
+
+    console.log(yAuth);
+
+    // var token = yAuth.createToken(function(oauth_access_token) { data: 'data'});
+    var token = yAuth.createToken(function('access token'), {}
+      if (error) {
+        console.log("suppp");
+        console.log('error: ' + JSON.stringify(error))
+      } else {
+        console.log('oauth_token: ' + oauth_token);
+        oauth_token.sign({
+          url: 'https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nfl/teams',
+          method: 'GET',
+          oauth_token
+        }).then(function(error, results, response) {
+              if (error) {
+                console.log("hayy ln 41!!!");
+              } else {
+                console.log(response);
+              }
+        })
+      }
+    })
+  }
+
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/*{this.yahooAuth()}*/}
       </div>
     );
   }
